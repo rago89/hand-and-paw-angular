6825c9d6677abdafb205311d9eb5d62284195560
“Rafael <rago89@gmail.com>
2022-06-01 16:45:08 +0200

    set timeout lo login form error

--- src/app/components/user/forms/login-user/login-user.component.ts
+++ src/app/components/user/forms/login-user/login-user.component.ts
@@ -40,7 +40,9 @@ export class LoginUserComponent implements OnInit, OnDestroy {
         },
         error: (errorMessage) => {
           this.loginErrorMessage = errorMessage;
-          return EMPTY;
+          setTimeout(() => {
+            this.loginErrorMessage = '';
+          }, 200);
         },
         complete: () => {
           this.isFetching = false;
